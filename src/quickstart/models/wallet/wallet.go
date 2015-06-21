76349e047f7a0b1663265d80d12e6fc33ccf8db0
diao <diaochengdong@gmail.com>
2015-06-21 09:29:14 +0800

    could input the right money

--- src/quickstart/models/wallet/wallet.go
+++ src/quickstart/models/wallet/wallet.go
@@ -93,10 +93,10 @@ func InsertInput(RawTran RawTransaction, tx []btcjson.ListUnspentResult) (RawTra
 	return RawTran, nil
 }
 
-func InsertOutput(RawTran RawTransaction, Message string) (RawTransaction, uint64, error) {
+func InsertOutputMinMoney(RawTran RawTransaction, Message string) (RawTransaction, uint64, error) {
 	var m []byte = []byte(Message)
 	var output Output
-	fee := conf.COIN
+	fee := conf.MESSAGEFEE
 	output.Value = HashRevert(fmt.Sprintf("%.16x", conf.COIN))
 	output.ScriptPubKey = conf.PAYTOWISHINGWALL
 	output.ScriptLen = fmt.Sprintf("%x", len(output.ScriptPubKey)/2)
@@ -132,6 +132,44 @@ func InsertOutput(RawTran RawTransaction, Message string) (RawTransaction, uint6
 	return RawTran, fee, nil
 }
 
+func InsertOutputPay(RawTran RawTransaction, PaytoMe uint64, Message string) (RawTransaction, error) {
+	var m []byte = []byte(Message)
+	var output Output
+	output.Value = HashRevert(fmt.Sprintf("%.16x", PaytoMe))
+	output.ScriptPubKey = conf.PAYTOWISHINGWALL
+	output.ScriptLen = fmt.Sprintf("%x", len(output.ScriptPubKey)/2)
+	RawTran.OutputList = append(RawTran.OutputList, output)
+
+	// total[1] + index[1] + 31 + 33
+	var total = len(m) / 64
+
+	var tail = len(m) % 64
+	if tail != 0 {
+		total++
+		for i := 0; i < (64 - tail); i++ {
+			m = append(m, 0xff)
+		}
+	}
+
+	for i := 0; i < total; i++ {
+		base := i * 64
+		var message Output
+		message.Value = HashRevert(fmt.Sprintf("%.16x", conf.MESSAGEFEE))
+
+		message.ScriptPubKey = conf.OP_1 + "21" +
+			fmt.Sprintf("%.2x", total) + fmt.Sprintf("%.2x", i) +
+			fmt.Sprintf("%x", string(m[base:base+31])) + "21" +
+			fmt.Sprintf("%x", string(m[base+31:base+64])) +
+			conf.OP_2 + conf.OP_CHECKMULTISIG
+
+		message.ScriptLen = fmt.Sprintf("%x", len(message.ScriptPubKey)/2)
+		RawTran.OutputList = append(RawTran.OutputList, message)
+	}
+	RawTran.OutputCount = fmt.Sprintf("%.2x", total+1)
+
+	return RawTran, nil
+}
+
 var connCfg = &btcrpcclient.ConnConfig{
 	Host:         "192.168.31.104:19011",
 	User:         "admin2",
@@ -155,23 +193,33 @@ func CreateRawTransaction(PayAddress string, Message string) (serialize string, 
 			totalmoney += uint64(temp.Amount * float64(conf.COIN))
 		}
 	}
-	RawTran := GetEmptyRawTransacton()
-	RawTran, _ = InsertInput(RawTran, tx)
 
-	RawTran, _, _ = InsertOutput(RawTran, Message)
-	serialize, _ = RawTran.ToSerialize()
-	MsgTx, _, _ := BlockChain.SignRawTransactionCMD(serialize)
-	fmt.Printf("Serial is %v\r\n", serialize)
-	fmt.Printf("MsgTx is %#v\r\n", MsgTx)
-	hash, err := BlockChain.SendRawTransaction(MsgTx, true)
-	fmt.Printf("hash is %v err is %v\r\n", hash, err)
+	RawTranTry := GetEmptyRawTransacton()
+	RawTranTry, _ = InsertInput(RawTranTry, tx)
 
-	/*
-		if totalmoney < 10*conf.COIN {
-			return "", errors.New(fmt.Sprintf("Do not Have enough money in address %s has %v need %v", PayAddress,
-				totalmoney, 10*conf.COIN))
-		}
-	*/
-	return serialize, 0, nil
+	RawTranTry, MinMoney, _ := InsertOutputMinMoney(RawTranTry, Message)
+	serializeTry, _ := RawTranTry.ToSerialize()
+	MsgTx, _, _ := BlockChain.SignRawTransactionCMD(serializeTry)
+
+	MinMoney += uint64(MsgTx.SerializeSize()) / 1000 * conf.FEE
+
+	//hash, err := BlockChain.SendRawTransaction(MsgTx, true)
+	//fmt.Printf("hash is %v err is %v\r\n", hash, err)
+
+	if totalmoney < MinMoney {
+		return "", MinMoney, errors.New(fmt.Sprintf("Do not Have enough money in address %s has %v need %v", PayAddress,
+			totalmoney, MinMoney))
+	}
+
+	RawTranReal := GetEmptyRawTransacton()
+	RawTranReal, _ = InsertInput(RawTranReal, tx)
+
+	RawTranReal, _ = InsertOutputPay(RawTranReal, (totalmoney - MinMoney + conf.MESSAGEFEE), Message)
+	serializeReal, _ := RawTranReal.ToSerialize()
+	MsgTxReal, _, _ := BlockChain.SignRawTransactionCMD(serializeReal)
+	fmt.Printf("Msg serlen %v\r\n", MsgTxReal.SerializeSize())
+	MinMoney += uint64(MsgTxReal.SerializeSize()) / 1000 * conf.FEE
+	fmt.Printf("serialize is %v\r\n", serializeReal)
+	return serializeReal, MinMoney, nil
 
 }
